16176630a4020f0d8abe1a0d2024cdba7e7a3419
Silviu Ardelean <silviu.ardelean@gmail.com>
2017-10-14 08:45:11 +0300

    Fix for issue #1 - psa-lin is not using Unicode

--- include/generic_tree_handler.h
+++ include/generic_tree_handler.h
@@ -7,40 +7,25 @@
 #endif
 #include <locale>
 
-#define USE_CODECVT 0
-#define USE_IMBUE   1
-
-#if USE_CODECVT
-#include <codecvt>
-#endif 
+class ProcsTreeBuilder;
 
 template <typename T>
 class generic_tree_handler
 {
-
   public:
 
 	static void dfs_traverse(generic_node<T>* node, bool lastRN = false)
 	{
 		if (!node)
 			return;
+
 #ifdef _WIN32 
 		_setmode(_fileno(stdout), _O_U16TEXT);
-#elif __linux__
-		#if USE_CODECVT
-				std::locale en("en_US.utf8",
-					new codecvt_utf8<wchar_t, 0x10ffff, consume_header>{});
-		#else
-				std::locale en("en_US.utf8");
-		#endif
-		#ifdef USE_IMBUE
-				std::wcout.imbue(en);
-		#else
-				std::locale::global(en);
-		#endif
+#else
+//		setlocale(LC_ALL, "en_US.UTF-8");
+		std::wcout.sync_with_stdio(false);
+		std::wcout.imbue(std::locale("en_US.utf8"));
 #endif
-		//std::wiostream output;
-		//output.imbue(std::locale("en_US.utf8"));
 
 		// list of Unicode characters 
 		// http://www.fileformat.info/info/unicode/category/So/list.htm
@@ -68,21 +53,88 @@ class generic_tree_handler
 			}
 		}
 
-		std::wcout << node->data << _T("\n");
+		std::wcout << node->data;
 
 		for (auto it = node->listChildren.begin(); it != node->listChildren.end(); ++it)
 		{
 			auto it_temp = it;
 			it_temp++;
 
-			if (node->parent == nullptr 
-					&& it_temp == node->listChildren.end())
+			if (node->parent == nullptr
+				&& it_temp == node->listChildren.end())
 			{
-				
+
 				lastRN = true;	// the last child of the root
 			}
 
 			dfs_traverse(*it, lastRN);
+		}
+	}
+
+	void dfs_traverse_nonstatic(generic_node<T>* node, bool lastRN = false)
+	{
+		if (!node)
+			return;
+
+#ifdef _WIN32 
+		_setmode(_fileno(stdout), _O_U16TEXT);
+#else
+		//		setlocale(LC_ALL, "en_US.UTF-8");
+		std::wcout.sync_with_stdio(false);
+		std::wcout.imbue(std::locale("en_US.utf8"));
+#endif
+
+		if (node->level > 0)
+		{
+			if (node->level == 1)
+			{
+				wchar_t ch1 = 0x2514, ch2 = 0x2500, ch3 = 0x251C;
+
+				if (!lastRN)
+					std::wcout << ch3 << ch2 << ch2 << ch2 << _T(" ");			//std::wcout << "├───";
+				else
+					std::wcout << ch1 << ch2 << ch2 << ch2 << _T(" ");			//std::wcout << "└───";
+			}
+			else
+			{
+				bool first = true;
+				for (auto i = 1; i < node->level; i++)
+				{
+					// needs checking if there are other nodes on the same level and if yes then add 0x2502
+					// temporary comment to have a cleaner tree
+					//if (!lastRN && first) {
+					//	first = false;
+					//wchar_t ch2 = 0x2502;
+					//	std::wcout << ch2 << _T("    ");				//std::wcout << "|";
+					//}
+					//else
+						std::wcout << _T("    ");
+				}
+
+				wchar_t ch1 = 0x2514, ch2 = 0x2500, ch3 = 0x251C;
+
+				std::wcout << ch1 << ch2 << ch2 << ch2 << _T(" ");			//std::wcout << "└───";	├───
+			}
+		}
+
+		if (parent)	
+			parent->print_it(node);
+		else 
+			std::wcout << node->data;
+
+		for (auto it = node->listChildren.begin(); it != node->listChildren.end(); ++it)
+		{
+			auto it_temp = it;
+			it_temp++;
+
+			if (node->parent == nullptr
+				&& it_temp == node->listChildren.end())
+			{
+
+				lastRN = true;	// the last child of the root
+			}
+
+			dfs_traverse_nonstatic(*it, lastRN);
 		}
 	}
 
@@ -106,15 +158,23 @@ class generic_tree_handler
 					std::wcout << "     ";
 				}
 
-				std::wcout << _T("+---- ");
+				std::wcout << _T("+--- ");
 			}
 		}
 
-		std::wcout << node->data << _T("\n");
+		std::wcout << node->data;
 
 		for (auto it = node->listChildren.begin(); it != node->listChildren.end(); ++it)
 		{
 			dfs_traverse_nonunicode(*it);
 		}
 	}
+	
+	void set_parent(ProcsTreeBuilder *_parent)
+	{
+		parent = _parent;
+	}
+
+ private:
+	ProcsTreeBuilder *parent = nullptr;
 };
--- src/psa-lin/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-
-int main()
-{
-    printf("hello from psa_lin!\n");
-    return 0;
-}
