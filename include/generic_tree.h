47c2a27d662fd8253ddb071f2be2f665bfb25960
Silviu Ardelean <5840603+SilviuArdelean@users.noreply.github.com>
2026-05-25 17:05:02 +0200

    Add proper copyright

--- include/generic_tree.h
+++ include/generic_tree.h
@@ -1,4 +1,26 @@
-﻿#pragma once
+﻿/*
+ * Copyright (c) 2017-2026 Silviu-Marius Ardelean
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
 
 #include <list>
 #include <memory>
@@ -72,7 +94,7 @@ class generic_tree {
 
   ~generic_tree() { tree_cleaner(root_); }
 
-  [[nodiscard]] generic_node<T>* add(generic_node<T>* _parent, const T& _data) {
+  generic_node<T>* add(generic_node<T>* _parent, const T& _data) {
     generic_node<T>* pnew = new_node(_parent, _data);
     _parent->listChildren.push_back(pnew);
 
